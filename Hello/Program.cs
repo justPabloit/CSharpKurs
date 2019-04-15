63f54b302f4fb35c791d9a5cc5aa83e5dbc8f52e
Paweł Piekutowski <futureiscode@gmail.com>
2019-04-15 14:47:50 +0200

    Witaj Swiecie

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            Console.WriteLine("Witaj Świecie");
+            Console.ReadLine(); 
         }
     }
 }
